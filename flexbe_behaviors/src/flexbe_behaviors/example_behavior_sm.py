921d71cd7ee75edd9aa83ff3bc306dba5472efec
Alireza <alireza.mixedreality@gmail.com>
2018-02-13 13:21:58 +0100

    refactor: use sncn_flexbe_states in example behavior

--- flexbe_behaviors/src/flexbe_behaviors/example_behavior_sm.py
+++ flexbe_behaviors/src/flexbe_behaviors/example_behavior_sm.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 ###########################################################
 #               WARNING: Generated code!                  #
 #              **************************                 #
@@ -6,9 +7,9 @@
 # Only code inside the [MANUAL] tags will be kept.        #
 ###########################################################
 
-from flexbe_core import Behavior, Autonomy, OperatableStateMachine, Logger
-from flexbe_states.log_state import LogState
-from flexbe_states.wait_state import WaitState
+from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from sncn_flexbe_states.undock_state import UndockState
+from sncn_flexbe_states.reset_localization_state import ResetLocalizationState
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -16,65 +17,66 @@ from flexbe_states.wait_state import WaitState
 
 
 '''
-Created on Fri Aug 21 2015
-@author: Philipp Schillinger
+Created on Tue Feb 13 2018
+@author: Synapticon GmbH
 '''
 class ExampleBehaviorSM(Behavior):
-	'''
-	This is a simple example for a behavior.
-	'''
+    '''
+    This is a simple example for a behavior.
+    '''
 
 
-	def __init__(self):
-		super(ExampleBehaviorSM, self).__init__()
-		self.name = 'Example Behavior'
+    def __init__(self):
+        super(ExampleBehaviorSM, self).__init__()
+        self.name = 'Example Behavior'
 
-		# parameters of this behavior
-		self.add_parameter('waiting_time', 3)
+        # parameters of this behavior
 
-		# references to used behaviors
+        # references to used behaviors
 
-		# Additional initialization code can be added inside the following tags
-		# [MANUAL_INIT]
+        # Additional initialization code can be added inside the following tags
+        # [MANUAL_INIT]
 		
 		# [/MANUAL_INIT]
 
-		# Behavior comments:
+        # Behavior comments:
 
-		# O 172 147 
-		# This transition will only be executed if the Autonomy Level is greater than Low during execution, e.g. High
+        # O 411 144 
+        # This transition will only be executed if the Autonomy Level is greater than Low during execution, e.g. High
 
 
 
-	def create(self):
-		log_msg = "Hello World!"
-		# x:83 y:390
-		_state_machine = OperatableStateMachine(outcomes=['finished'])
+    def create(self):
+        # x:617 y:333, x:63 y:408
+        _state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
+        _state_machine.userdata.undock_backward_dist = 0.5
+        _state_machine.userdata.undock_inplace_rot_angle = 0.0
 
-		# Additional creation code can be added inside the following tags
-		# [MANUAL_CREATE]
+        # Additional creation code can be added inside the following tags
+        # [MANUAL_CREATE]
 		
 		# [/MANUAL_CREATE]
 
 
-		with _state_machine:
-			# x:52 y:78
-			OperatableStateMachine.add('Print_Message',
-										LogState(text=log_msg, severity=Logger.REPORT_HINT),
-										transitions={'done': 'Wait_After_Logging'},
-										autonomy={'done': Autonomy.Low})
+        with _state_machine:
+            # x:140 y:117
+            OperatableStateMachine.add('reset_localization',
+                                        ResetLocalizationState(),
+                                        transitions={'finished': 'undock', 'failed': 'failed'},
+                                        autonomy={'finished': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:40 y:228
-			OperatableStateMachine.add('Wait_After_Logging',
-										WaitState(wait_time=self.waiting_time),
-										transitions={'done': 'finished'},
-										autonomy={'done': Autonomy.Off})
-
-
-		return _state_machine
+            # x:341 y:245
+            OperatableStateMachine.add('undock',
+                                        UndockState(),
+                                        transitions={'done': 'finished', 'failed': 'failed'},
+                                        autonomy={'done': Autonomy.Off, 'failed': Autonomy.Off},
+                                        remapping={'backward_distance': 'undock_backward_dist', 'in_place_rotation_angle': 'undock_inplace_rot_angle'})
 
 
-	# Private functions can be added inside the following tags
-	# [MANUAL_FUNC]
+        return _state_machine
+
+
+    # Private functions can be added inside the following tags
+    # [MANUAL_FUNC]
 	
 	# [/MANUAL_FUNC]
